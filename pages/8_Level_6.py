6cd7578d3daaf5c3106009401a8d50a70b345706
Alice Heiman <84677675+aliceheiman@users.noreply.github.com>
2023-01-22 12:18:58 +0100

    update level 6

--- pages/8_Level_6.py
+++ pages/8_Level_6.py
@@ -3,9 +3,9 @@ from template import *
 # Configuration
 level = {
     "level": "Level 6",
-    "new_symbols": "SR",
-    "new_label": "SR",
-    "all_label": "ETASILONUDSR",
+    "new_symbols": "RH",
+    "new_label": "RH",
+    "all_label": "ETASILONUDRH",
     "length_unit": 5,
     "num_units_tutorial": 5,
     "num_units_all": 7,
